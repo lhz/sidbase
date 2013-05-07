3f89902c1cc73965a5a11916a445222c8311cd54
Lars Haugseth <lars@larshaugseth.com>
2013-05-07 21:26:00 +0200

    Added route for OPTIONS request.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Sidbase::Application.routes.draw do
+  match '/api/v1/:model'     => 'api#options', :via => 'options'
   match '/api/v1/:model'     => 'api#index',   :via => 'get'
   match '/api/v1/:model/:id' => 'api#show',    :via => 'get'
 end
